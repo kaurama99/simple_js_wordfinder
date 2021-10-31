4dd0c0276f690673aabec13dbdc55f7099e7303f
Kaushik Ramani <kaushikramain@marzvfx.com>
2021-10-31 12:58:45 -0400

    created folder structure and basic components

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+  app = express(),
+  port = process.env.PORT || 8081;
+
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + "/public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+var routes = require("./routes/routes");
+
+app.use("/", routes);
+
+app.listen(port);
+console.log("WordFinder running on port: " + port);
